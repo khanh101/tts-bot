cfe41f07462464f89715fc2fb97d7c710776e48a
khanh <nguyenngockhanh.pbc@gmail.com>
2021-06-13 17:29:32 +0800

    added blocked user

--- handler/handler.py
+++ handler/handler.py
@@ -52,7 +52,8 @@ async def handle_message(client: discord.Client, message: discord.Message):
     if message.author.bot:
         return
     # filter out blocked user
-    if message.author in get_blocked_user():
+    if message.author.discriminator in get_blocked_user():
+        message.channel.send(f"WARNING: User {message.author.name}#{message.author.discriminator} has been blocked from using the bot")
         return
 
     server_id = int(message.guild.id)
