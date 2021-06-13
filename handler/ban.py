79c63f719775cbba951c697d4ada0e8f18acddfd
khanh <nguyenngockhanh.pbc@gmail.com>
2021-06-13 21:33:54 +0800

    refactored

--- handler/ban.py
+++ handler/ban.py
@@ -1,9 +1,9 @@
 import discord.member
 
-BLOCK_FILENAME = "block.txt"
+BAN_FILENAME = "ban.txt"
 
 
-def is_blocked(author: discord.member.Member) -> bool:
-    with open(BLOCK_FILENAME, "r") as f:
+def is_banned(author: discord.member.Member) -> bool:
+    with open(BAN_FILENAME, "r") as f:
         blocked = set(map(lambda line: line[:-1] if line[-1] == "\n" else line, f.readlines()))
         return author.discriminator in blocked
--- handler/handler.py
+++ handler/handler.py
@@ -3,7 +3,7 @@ from typing import Dict
 
 import discord
 
-from handler.block import is_blocked
+from handler.ban import is_banned, BAN_FILENAME
 from handler.command import say_text, set_lang, say_line, TTS_CHANNEL, LINE_FOLDERNAME
 from handler.config import Config
 
@@ -32,6 +32,12 @@ async def howto(config: Config, client: discord.Client, message: discord.message
     help_message += "LINE AVAILABLE:\n"
     for filename in os.listdir(LINE_FOLDERNAME):
         help_message += f"\t{'.'.join(filename.split('.')[:-1])}"
+    help_message += "\n"
+    help_message += "BAN LIST:\n"
+    with open(BAN_FILENAME, "r") as f:
+        for line in f.readlines():
+            line = line[:-1] if line[-1] == "\n" else line
+            help_message += f"\t{line}"
 
     await message.channel.send(help_message)
 
@@ -56,7 +62,7 @@ async def handle_message(client: discord.Client, message: discord.Message):
     for k in command.keys():
         if message.content == k:
             # filter out blocked user
-            if is_blocked(message.author):
+            if is_banned(message.author):
                 await message.delete()
                 await message.channel.send(
                     f"WARNING: User {message.author.name}#{message.author.discriminator} has been banned")
@@ -69,7 +75,7 @@ async def handle_message(client: discord.Client, message: discord.Message):
     for k in command_with_args.keys():
         if message.content.startswith(k + " "):
             # filter out blocked user
-            if is_blocked(message.author):
+            if is_banned(message.author):
                 await message.delete()
                 await message.channel.send(
                     f"WARNING: User {message.author.name}#{message.author.discriminator} has been banned")
@@ -85,7 +91,7 @@ async def handle_message(client: discord.Client, message: discord.Message):
     # tts channel
     if message.channel.name == TTS_CHANNEL:
         # filter out blocked user
-        if is_blocked(message.author):
+        if is_banned(message.author):
             await message.delete()
             await message.channel.send(
                 f"WARNING: User {message.author.name}#{message.author.discriminator} has been banned")
